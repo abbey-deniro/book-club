73f9cd89b3ce97713d27f810b88a50b7c60772bf
Nirvik Sharma <nirviks16@icloud.com>
2023-02-23 22:55:11 -0700

    Installed Npm and Added Frontend folder

--- Frontend/src/Login.js
+++ Frontend/src/Login.js
@@ -1,42 +1,3 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-
-
-const theme = createTheme({
-    palette: {
-        primary: {
-            main: '#314772',
-        },
-        secondary: {
-            main: '#4D6FB3',
-        },
-        darkest: {
-            main: "#00171F",
-        }
-    }
-});
-
-export default function SignIn() {
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const data = new FormData(event.currentTarget);
-        console.log({
-            email: data.get('email'),
-            password: data.get('password'),
-        });
-    };
 
     return (
         <ThemeProvider theme={theme}>
@@ -44,7 +5,7 @@ export default function SignIn() {
                 <CssBaseline />
                 <Box
                     sx={{
-                        marginTop: 10,
+                        marginTop: 25,
                         display: 'flex',
                         flexDirection: 'column',
                         alignItems: 'center',
@@ -78,7 +39,7 @@ export default function SignIn() {
                             autoComplete="current-password"
                         />
                         <FormControlLabel
-                            control={<Checkbox value="remember" color="secondary" />}
+                            control={<Checkbox value="remember" color="primary" />}
                             label="Remember me"
                         />
                         <Button
@@ -91,12 +52,12 @@ export default function SignIn() {
                         </Button>
                         <Grid container>
                             <Grid item xs>
-                                <Link href="#" variant="body2" color="darkest.main">
+                                <Link href="#" variant="body2">
                                     Forgot password?
                                 </Link>
                             </Grid>
                             <Grid item>
-                                <Link href="#" variant="body2" color="darkest.main">
+                                <Link href="/signup" variant="body2">
                                     {"Don't have an account? Sign Up"}
                                 </Link>
                             </Grid>
