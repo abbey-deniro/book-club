cc0400513878e913221d42239d33c9c383e7b704
Nirvik Sharma <nirviks16@icloud.com>
2023-02-23 22:55:11 -0700

    Installed Npm and Added Frontend folder

--- Frontend/src/Signup.js
+++ Frontend/src/Signup.js
@@ -118,9 +118,9 @@ export default function SignUp() {
                         >
                             Sign Up
                         </Button>
-                        <Grid container justifyContent="flex-end">
+                        <Grid container  justifyContent ="center">
                             <Grid item>
-                                <Link href="#" variant="body2">
+                                <Link href="/login" variant="body2">
                                     Already have an account? Sign in
                                 </Link>
                             </Grid>
