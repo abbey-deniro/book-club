30b8cc59e75538ba8dba30d75a4b4ca70ad61865
DanielG-N <dagonzalez@student.neumont.edu>
2023-03-07 10:46:51 -0700

    fix user get clubs

--- Backend/UserService/UserClubs/getUserClubs.py
+++ Backend/UserService/UserClubs/getUserClubs.py
@@ -1,8 +1,20 @@
 from pymongo import MongoClient
+import re
+import jwt
 
 client = MongoClient('mongodb+srv://team1:fV7v57oPgnrrhiiP@theclub.bzkoudj.mongodb.net/?retryWrites=true&w=majority')
 db = client['Bookclub']['Clubs']
 
 def lambda_handler(event, context):
-    return list(db.find({'$or' : [{'Owner' : event['userEmail']}, {'members':event['userEmail']}]}))
+    token = ''
+    headers = dict(event['headers'])
+    try:
+        auth_header = headers['Authorization']
+        token = re.match("Bearer (.+)", auth_header)[1]
+    except:
+        auth_header = headers['authorization']
+        token = re.match("Bearer (.+)", auth_header)[1]
+    
+    user = jwt.decode(token, "team1bookclubthebest", algorithms="HS256")
+    return list(db.find({'$or' : [{'Owner' : user['_id']}, {'members':user['_id']}]}))
 
