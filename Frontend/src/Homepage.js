9af8b5da3c0f8dbbac1d00ceba5b7b0199f20642
satanscorner <adeniro@student.neumont.edu>
2023-03-07 19:22:15 -0700

    homepage styling

--- Frontend/src/Homepage.js
+++ Frontend/src/Homepage.js
@@ -91,10 +91,14 @@ function DashboardContent() {
                   minHeight: 250,
                 }}
               >
-                <Typography component="h1" variant="h4">
+                <Typography component="h1" variant="h4" paddingBottom={ '2rem' }>
                   Welcome, {user.username}
                 </Typography>
                 {/* Book club stack */}
+                <Stack spacing={3} direction="column" justifyContent='space-evenly' >
+                  <Button variant="contained" href="/joinClub" color="primary" style={{ textTransform: "none", letterSpacing: "2px" }}>Join New Club</Button>
+                  <Button variant="contained" href="/createClub" color="primary" style={{ textTransform: "none", letterSpacing: "2px" }}>Create New Club</Button>
+                </Stack>
               </Paper>
             </Grid>
             {/* Calendar */}
@@ -111,14 +115,14 @@ function DashboardContent() {
               </Paper>
             </Grid>
             {/* Buttons on row */}
-            <Grid item xs={12}>
+            {/* <Grid item xs={12}>
               <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
                 <Stack spacing={1} direction="row" justifyContent='space-evenly' >
                   <Button variant="contained" href="/joinClub" color="primary" style={{ textTransform: "none", width: "30rem", letterSpacing: "2px" }}>Join New Club</Button>
                   <Button variant="contained" href="/createClub" color="primary" style={{ textTransform: "none", width: "30rem", letterSpacing: "2px" }}>Create New Club</Button>
                 </Stack>
               </Paper>
-            </Grid>
+            </Grid> */}
           </Grid>
           <Grid>
             <Grid item xs={12}>
@@ -127,15 +131,24 @@ function DashboardContent() {
                   clubs.map(club => {
                     return (
                       <Grid item xs={1}
-                        style={{
-                          height: "500px",
-                          width: "575px",
+                        // style={{
+                        //   height: "400px",
+                        //   width: "575px",
+                        //   display: 'inline',
+                        //   float: "left"
+                        // }}>
+                        sx={{
+                          p: 2,
+                          // paddingTop: "1rem",
+                          maxHeight: "400px",
+                          minWidth: "555px",
                           display: 'inline',
                           float: "left"
                         }}>
-                        <Paper sx={{ p: 10, display: 'flex', flexDirection: 'column', margin: "20" }}>
-                          <img src={club.Image} width="100%" height="400px" name="bookImage" />
-                          <Button variant="contained" href={'/club/' + club._id} color="secondary" style={{}}>{club.Name}: {club.BookTitle}</Button>
+
+                        <Paper sx={{ p: 9, display: 'flex', flexDirection: 'column', margin: "20"}}>
+                          <img src={club.Image} width="400px" height="400px" name="bookImage" />
+                          <Button marginTop="15px" variant="contained" href={'/club/' + club._id} color="secondary" style={{ marginTop: '1rem' }}>{club.Name}: {club.BookTitle}</Button>
                         </Paper>
 
                       </Grid>
